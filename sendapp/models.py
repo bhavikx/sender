1aa8bc47bb879601bd77d6c88cbf71cadc66d636
bhavikx <64426653+bhavikx@users.noreply.github.com>
2020-06-11 14:09:13 +0530

    ---

--- sendapp/models.py
+++ sendapp/models.py
@@ -4,35 +4,35 @@ from django.contrib.auth.models import User
 class UserPro(models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
 
-	name = models.CharField(max_length = 30, null = False)
+	name = models.CharField(max_length = 30, blank = False)
 	def __str__(self):
 		return self.name	
 
 class EmailFromList(models.Model):
-	user = models.ForeignKey(UserPro, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
 
-	userEmail = models.EmailField(max_length = 200, null = False)
-	userPassword = models.CharField(max_length = 32, null = False)
+	userEmail = models.EmailField(max_length = 200, blank = False)
+	userPassword = models.CharField(max_length = 32, blank = False)
 	isDefault = models.BooleanField(default = False)
 
 	def __str__(self):
 		return self.userEmail
 
 class EmailDraft(models.Model):
-	user = models.ForeignKey(UserPro, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
 
-	emailDraftSubject = models.CharField(max_length = 40, null = False)
-	emailDraftMessage = models.TextField(max_length = 300, null = False)
+	emailDraftSubject = models.CharField(max_length = 40, blank = False)
+	emailDraftMessage = models.TextField(max_length = 300, blank = False)
 	timeDraftCreated = models.DateTimeField(auto_now_add = True)
 
 	def __str__(self):
 		return self.emailDraftSubject
 	
 class EmailList(models.Model):
-	user = models.ForeignKey(UserPro, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
 
-	emailListName = models.CharField(max_length = 20, null = False)
-	emailList = models.CharField(max_length = 2500)
+	emailListName = models.CharField(max_length = 20, blank = False)
+	emailList = models.CharField(max_length = 2500, blank = False)
 	selectedEmailList = models.BooleanField(default = False)
 
 	def __str__(self):
