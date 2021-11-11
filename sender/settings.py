19b5cb8f0d25958aeb5d1a29ce332fc93227d16a
bhavikx <64426653+bhavikx@users.noreply.github.com>
2021-11-11 17:56:07 +0530

    Update settings.py

--- sender/settings.py
+++ sender/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtpl.live.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhavikxx@hotmail.com'
-EMAIL_HOST_PASSWORD = 'bone0299'
+EMAIL_HOST_USER = 'email here'
+EMAIL_HOST_PASSWORD = 'password here'
 
 #DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
